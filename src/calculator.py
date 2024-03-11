9f80539fba8321c987fce60efb9baaa4b9880447
reikan75 <reikanxvicontact@gmail.com>
2024-03-11 22:20:28 +0100

    Add min function

--- src/calculator.py
+++ src/calculator.py
@@ -2,3 +2,7 @@ class Calculator :
     # return the sum between 2 operands
     def mysum(self, first_operand, second_operand):
         return first_operand + second_operand
+
+    # return the minimum between 2 operands
+    def mymin(self, first_operand, second_operand):
+        return first_operand if second_operand > first_operand else second_operand
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,9 +2,16 @@ from unittest import TestCase
 from src.calculator import Calculator
 
 class Test_Calculator(TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
     def test_sum(self):
-        calc = Calculator()
-        self.assertEqual(calc.mysum(1, 2), 3)
+        self.assertEqual(self.calc.mysum(1, 2), 3)
+
+    def test_min(self):
+        self.assertEqual(self.calc.mymin(1, 2), 1)
+
+    
     
 if __name__ == '__main__':
     unittest.main()
