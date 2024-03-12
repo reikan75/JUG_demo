e9794fca1a3fe12ccd02ee0f6fcc47556d153168
reikan75 <reikanxvicontact@gmail.com>
2024-03-12 19:51:40 +0100

    Add max function

--- src/calculator.py
+++ src/calculator.py
@@ -8,5 +8,5 @@ class Calculator :
         return first_operand if second_operand > first_operand else second_operand
 
     # return the maximum between 2 operands
-    def mymin(self, first_operand, second_operand):
+    def mymax(self, first_operand, second_operand):
         return first_operand if second_operand < first_operand else second_operand
